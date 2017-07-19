cd405e611658031655f80d7c6b1d13706e78309c
admin <18200209182@163.com>
2017-07-19 16:10:00 +0800

    vue-dome

--- src/components/common/line.vue
+++ src/components/common/line.vue
@@ -6,7 +6,23 @@
   </div>
 </template>
 <style lang="less" scoped rel="stylesheet/less">
-    @import "line.less";
+  @import "../../../static/less/mixin.less";
+  .line-title{
+    display: flex;
+    margin:0 36px 0 36px;
+    padding:30px 0 24px 0;
+    .line{
+      flex: 1;
+      position: relative;
+      top: -6px;
+    .border-1px(bottom,rgba(255,255,255,.2));
+    }
+    .title-text{
+      padding: 0 12px;
+      font-size: 14px;
+      font-weight:700;
+    }
+  }
 </style>
 <script type="text/ecmascript-6">
     export default{
